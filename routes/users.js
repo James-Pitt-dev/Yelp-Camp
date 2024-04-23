b4ee2b41eae352ba8dc5a8aba3151ee54737656d
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2024-04-22 17:22:17 -0700

    Added registration form. Added get/post route

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,17 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 })
 
+router.post('/register', catchAsync(async (req, res) => {  
+    try {
+        const {email, username, password} = req.body.campground;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp! ' + user.username);
+        res.redirect('/campgrounds');
+    } catch(e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
+
 module.exports = router;
