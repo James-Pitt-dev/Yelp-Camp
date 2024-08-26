aadffa4ff2e36cff7061356a650153839763f056
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2024-08-26 16:25:55 -0700

    Finished MapBox integration

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
-require('dotenv').config();
 const dbPassword = process.env.DATABASE_PASSWORD;
 const userID = process.env.USER_ID;
-mongoose.connect(dbPassword, {})
+mongoose.connect('mongodb+srv://jamespitt1:cTiHNKFp4QSL9x6B@cluster0.eimml8f.mongodb.net/?retryWrites=true&w=majority')
     .then(() => {
         console.log(`Connected to DB: ${mongoose.connection.db.databaseName}`);
     })
@@ -28,16 +28,21 @@ const seedDB = async () => {
     //Make the DB empty
     await Campground.deleteMany({});
     //Random entries img:483251
-   for(let i =0; i<50;i++){
+   for(let i =0; i<200;i++){
     const random1000 = Math.floor(Math.random() * 1000)
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-        author: userID,
+        author: '6629c348b8f425604264885a',
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
         title: `${sample(descriptors)} ${sample(places)}`,
+        price, 
+        geometry: {
+            type:"Point",
+            coordinates:[cities[random1000].longitude, cities[random1000].latitude]
+        },
         images: [
             {
-              url: 'https://res.cloudinary.com/dnyzy8xh1/image/upload/v1715288265/Yelpcamp/rpubwsgo7cb0mpoxr0he.jpg',
+              url: 'https://res.cloudinary.com/dnyzy8xh1/image/upload/v1715288265/Yelpcamp/rgidyhsg7nehqjor5ayn.jpg',
               filename: 'Yelpcamp/rpubwsgo7cb0mpoxr0he'
             },
             {
@@ -47,7 +52,6 @@ const seedDB = async () => {
           ],
         description: `
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda accusantium non dolorem veritatis ipsam corporis quibusdam at, quas repellat doloremque, quo distinctio sit, quaerat optio illo blanditiis! Sunt, architecto nulla?`,
-        price 
     })
     await camp.save();
    }    
